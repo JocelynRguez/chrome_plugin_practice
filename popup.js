f7f10850ea08002c15d301c94394d49272da42de
Jocelyn Rodriguez <jocelyn.rodriguez@pomona.edu>
2017-06-22 22:13:54 -0700

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,11 +1,41 @@
 // popup.js
 
-function listClick() {
-	document.getElementById("list").innerHTML = 'it works';
+document.getElementById("description").innerHTML = "counter: " + localStorage.current;
+
+
+for (var i = 0; i< localStorage.length; i++){
+	console.log( localStorage.key(i) );
 }
 
-document.getElementById("list").addEventListener("click", listClick);
 
+function listClick() {
+	if(localStorage.current){
+		localStorage.current = Number(localStorage.current) + 1;
+	}else{
+		localStorage.current = 1;
+	}
+	
+	document.getElementById("description").innerHTML = "counter: " + localStorage.current;
+}
+
+function openOptions(){
+	window.open("background.html");
+}
+
+function addSite(){
+	var site = document.URL; 
+}
+
+document.getElementById("button_list").addEventListener("click", listClick);
+document.getElementById("options").addEventListener("click",openOptions);
+document.getElementById("good").addEventListener("click", addSite)
+
+
+window.onunload = function () {
+	console.log("Hello");
+	//this will remove current each time popup closes
+	//localStorage.removeItem("current");
+}
 //var list = document.getElementById("list");
 
 
